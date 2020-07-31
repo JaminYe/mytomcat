e53ba3d53608738a26e41765faab3eee51e54156
JaminYe <JaminYe2018@outlook.com>
2020-07-31 18:07:00 +0800

    1

---
+++ src/util/MyRequest.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.io.InputStream;
+
+/**
+ * @author jamin
+ * @date 2020/7/31 18:03
+ * 请求类
+ */
+public class MyRequest {
+   /* *//**
+    *请求url
+    *//*
+    private String url;
+    *//**
+    *方法
+    *//*
+    private String method;*/
+    public MyRequest(InputStream inputStream){
+        byte[] httpRequestBytes = new byte[2048];
+    
+    }
+
+}
